93a26b1ee4aa65e2a142f05ebdc1383b7f354222
life4cut_cu <dev_cu@life4cut.com>
2022-04-07 14:02:45 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+//Dom -> document object model html문서를 가져와서 제어할수있다.
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginbutton = document.querySelector("button");
+
+loginbutton.addEventListener("click", login)
+
+function login(){
+const req = { //오브젝트
+  id : id.value,
+  psword : pw.value,
+};
+console.log(req);
+}
